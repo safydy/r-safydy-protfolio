d0c821fb44f08d87fedddf192eae77ca9e2a9d02
safydy <r.safydy@gmail.com>
2024-07-30 11:43:50 -0600

    add real awards

--- src/components/react_components/Award.jsx
+++ src/components/react_components/Award.jsx
@@ -1,21 +1,22 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import './Award.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { library, config } from '@fortawesome/fontawesome-svg-core';
-import { faAward, faTrophy, faMedal } from '@fortawesome/free-solid-svg-icons';
+import { faAward, faTrophy, faMedal, faUserGraduate, faGem } from '@fortawesome/free-solid-svg-icons';
 
 config.autoAddCss = false;
-library.add(faAward, faTrophy, faMedal);
+library.add(faAward, faTrophy, faMedal, faUserGraduate, faGem);
 
-const Award = ({ title, year, faIcon }) => {
+const Award = ({ title, subTitle, faIcon }) => {
   return (
-    <Box display="flex" alignItems="center" gap={2}>
+    <Box className="award" display="flex" alignItems="center" gap={2}>
       <FontAwesomeIcon icon={faIcon ?? "fa-solid fa-award"} />
-      <Box>
-        <Typography variant="h5" fontWeight="bold">{title}</Typography>
-        <Typography variant="h6" color="textSecondary">{year}</Typography>
+      <Box className="award-content">
+        <Typography variant="h5" fontWeight="bold" className="content-text">{title}</Typography>
+        <Typography variant="h6">{subTitle}</Typography>
       </Box>
     </Box>
   );
