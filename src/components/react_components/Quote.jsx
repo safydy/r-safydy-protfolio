9304de127c8330a6dd584cd198d876748e1bd8ea
safydy <r.safydy@gmail.com>
2024-07-30 12:09:23 -0600

    add quote icon on Quote.jsx

--- src/components/react_components/Quote.jsx
+++ src/components/react_components/Quote.jsx
@@ -6,13 +6,25 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import './Quote.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { library, config } from '@fortawesome/fontawesome-svg-core';
+import { faQuoteLeft, faQuoteRight } from '@fortawesome/free-solid-svg-icons';
+
+config.autoAddCss = false;
+library.add(faQuoteLeft, faQuoteRight);
+
 function Quote({ avatar, quote, author }) {
   return (
     <Card className="mention-quote">
-      <CardContent>
-        <Typography variant="h6" component="div" align="center" style={{ fontStyle: 'italic' }}>
-          " {quote} "
-        </Typography>
+      <CardContent className="quote-content">
+        <div className="quote-text">
+          <FontAwesomeIcon className="quote-icon icon-left" icon="fa-solid fa-quote-left"/>
+          <Typography variant="h6" component="div" align="center" style={{ fontStyle: 'italic' }}>
+            {quote}
+          </Typography>
+          <FontAwesomeIcon className="quote-icon icon-right" icon="fa-solid fa-quote-right"/>
+        </div>
         <Box display="flex" justifyContent="center" alignItems="center">
           <Typography variant="body2" component="div" sx={{ mr: 2 }}>
             {author}&nbsp;&nbsp;-
