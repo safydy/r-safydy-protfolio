0d21d87c0d54d67820325a7626cad1d571f701a7
safydy <r.safydy@gmail.com>
2024-08-05 12:54:04 -0600

    initi GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import netlify from "@astrojs/netlify";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [
+    react(),
+    partytown({
+      config: {
+        // Add the "dataLayer.push" as a forwarding-event with GA
+        forward: ['dataLayer.push'],
+      },
+    })
+  ],
   output: "server",
   adapter: netlify()
 });
